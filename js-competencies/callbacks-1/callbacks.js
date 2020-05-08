db333b539f74f5a946427f7ee81de55d55ed1553
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-08 15:08:30 -0600

    still working on the logic of callbacks

--- js-competencies/callbacks-1/callbacks.js
+++ js-competencies/callbacks-1/callbacks.js
@@ -3,6 +3,16 @@
 // Write a function called stringItOut that takes in an array. Determine if it is an array of numbers or strings and sort it greatest to least accordingly (sort strings alphabetically). Return the array converted into a string using the toString method.
 
 //Code here
+// function stringItOut(arr) {
+//   for(let i=0; i < arr.length; i++) {
+//     if(typeof arr[i] === "string") {
+//       arr[i].sort();
+//     }
+//     else if(typeof arr[i] === "number") {
+//       arr[i].sort()
+//     }
+//   }
+// }
 
 
 //////////////////PROBLEM 2////////////////////
